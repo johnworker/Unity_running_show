d6ee99b74574a43c6eefe55bba7746451559e85c
johnworker <besscomdark@gmail.com>
2022-06-09 21:17:48 +0800

    添加C#

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float horizontalmove;
+        horizontalmove = Input.GetAxis("Horizontal");
+
+        if(horizontalmove != 0)
+        {
+            rb.velocity = new Vector2(horizontalmove * speed, rb.velocity.y);
+        }
+    }
+}
