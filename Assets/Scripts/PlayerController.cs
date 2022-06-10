c9b8c5e57c1728d21aa9df37d21ea08424835059
johnworker <78348941+johnworker@users.noreply.github.com>
2022-06-10 17:28:55 +0800

    添加動畫

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator anim;
     public float speed;
     public float jumpforce;
 
@@ -26,9 +27,11 @@ public class PlayerController : MonoBehaviour
         float horizontalmove = Input.GetAxis("Horizontal");
         float facedircetion = Input.GetAxisRaw("Horizontal");
 
+        //®§¶‚≤æ∞ 
         if(horizontalmove != 0)
         {
             rb.velocity = new Vector2(horizontalmove * speed * Time.deltaTime, rb.velocity.y);
+            anim.SetFloat("running", Mathf.Abs(facedircetion));
         }
 
         if(facedircetion != 0)
@@ -36,6 +39,7 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector3(facedircetion, 1, 1);
         }
 
+        //®§¶‚∏ı≈D
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
