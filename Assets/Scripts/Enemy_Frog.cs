aa5872797390c2979961965337a21bc873125934
johnworker <78348941+johnworker@users.noreply.github.com>
2022-06-22 07:32:03 +0800

    敵人設定

--- Assets/Scripts/Enemy_Frog.cs
+++ Assets/Scripts/Enemy_Frog.cs
@@ -5,16 +5,46 @@ using UnityEngine;
 public class Enemy_Frog : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public Transform leftpoint, rightpoint;
+    public float Speed;
+
+    private bool Faceleft = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        transform.DetachChildren();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
+
+    void Movement()
+    {
+        if (Faceleft)
+        {
+            rb.velocity = new Vector2(-Speed, rb.velocity.y);
+            if(transform.position.x < leftpoint.position.x)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                Faceleft = false;
+            }
+        }
+
+        else
+        {
+            rb.velocity = new Vector2(Speed, rb.velocity.y);
+            if (transform.position.x > rightpoint.position.x)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                Faceleft = true;
+            }
+        }
+    }
+
+
 }
