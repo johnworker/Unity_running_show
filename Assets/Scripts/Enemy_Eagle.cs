75264f2238c82e6abdff5dd7f6fb8674cbf63784
johnworker <78348941+johnworker@users.noreply.github.com>
2022-06-24 07:26:38 +0800

    老鷹基本語法撰寫

---
+++ Assets/Scripts/Enemy_Eagle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Eagle : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Collider2D coll;
+    public Transform top, bottom;
+    public float Speed;
+    public float TopY, BottomY;
+
+    private bool isUp;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
