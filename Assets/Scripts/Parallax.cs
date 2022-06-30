cba98b217f5aa60bcc239abf431f34d41bd70419
johnworker <78348941+johnworker@users.noreply.github.com>
2022-06-30 20:11:14 +0800

    背景移動測試

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Transform Cam;
+    public float moveRate;
+    private float startPoint;
+    void Start()
+    {
+        startPoint = transform.position.x;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector2(startPoint + Cam.position.x * moveRate, transform.position.y);
+    }
+}
