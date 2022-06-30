fa314a95904a84af3afe669501e0c0ede878dfa4
johnworker <78348941+johnworker@users.noreply.github.com>
2022-07-01 06:48:31 +0800

    主選單效果

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -9,4 +9,14 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void UIEnable()
+    {
+        GameObject.Find("Canvas/MainMenu/UI").SetActive(true);
+    }
 }
