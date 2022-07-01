55c729de49ef091e0a5bdc0c87ed7991624c15c1
johnworker <78348941+johnworker@users.noreply.github.com>
2022-07-01 22:59:17 +0800

    遊戲暫停和回復

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject pauseMenu;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -19,4 +20,17 @@ public class Menu : MonoBehaviour
     {
         GameObject.Find("Canvas/MainMenu/UI").SetActive(true);
     }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
 }
