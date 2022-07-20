10a94d73e128137206dc4f24592cb68e8eb00d70
johnworker <78348941+johnworker@users.noreply.github.com>
2022-07-21 06:57:08 +0800

    添加結束圖片

--- Assets/Scripts/WinGame.cs
+++ Assets/Scripts/WinGame.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class WinGame : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            
+            Invoke("Restart", 1.5f);
         }
     }
 }
